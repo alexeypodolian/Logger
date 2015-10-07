f1e6ef160e5ee4994df148684a5845938986cfc1
alexeypodolian <alexeypodolian@gmail.com>
2015-10-07 09:33:01 +0300

    initial commit

---
+++ app/src/main/java/podolian/alexey/logger/MainActivity.java
@@ -0,0 +1,54 @@
+package podolian.alexey.logger;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import podolian.alexey.lib.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Logger.d("onCreate");
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Logger.d("fab clicked");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Logger.d("onCreateOptionsMenu");
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Logger.d("onOptionsItemSelected");
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Logger.d("action_settings clicked");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
